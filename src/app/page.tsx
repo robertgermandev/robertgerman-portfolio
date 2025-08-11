5e6d69db12a9972fed49f6a8a887e5e1ad218a1a
robertgermandev <robertgermandev@gmail.com>
2025-08-11 09:57:11 +0300

    removed default dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,7 @@ export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      setIsDarkMode(true);
-    } else {
-      setIsDarkMode(false);
-    }
+    setIsDarkMode(localStorage.theme === "dark");
   }, []);
 
   useEffect(() => {
